dd3bd1a7ae9bb2146ba0dad2f85033b34aecef49
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-11-07 12:58:57 +0000

    node modules unhide

--- ecommerce/next.config.js
+++ ecommerce/next.config.js
@@ -4,12 +4,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
-
 module.exports = nextConfig
